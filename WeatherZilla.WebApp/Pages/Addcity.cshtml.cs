efa637abfcb6649cc75bfb61a5598452bdde86d7
shalafi <jorgen@bertholdsson.com>
2022-05-15 19:55:48 +0200

    Added a form that takes a place and use that in backen code to present the searched place on the web page

--- WeatherZilla.WebApp/Pages/Addcity.cshtml.cs
+++ WeatherZilla.WebApp/Pages/Addcity.cshtml.cs
@@ -5,6 +5,7 @@ namespace WeatherZilla.WebApp.Pages
     public class AddcityModel : PageModel
     {
         public string CityName { get; set; }
+        public string Place { get; set; }
         private readonly IConfiguration _configuration;
 
         public AddcityModel(IConfiguration configuration)
@@ -16,6 +17,16 @@ namespace WeatherZilla.WebApp.Pages
         public void OnGet()
         {
             CityName = "Lycksele";
+            if (String.IsNullOrEmpty(Place))
+            {
+                Place = "No search yet";
+            }
+        }
+        public void OnPost()
+        {
+            var placeSearchedFor = Request.Form["place"];
+            Place = placeSearchedFor;
+            // TODO: Search weather provider for placeSearchedFor and add a valid place to database
         }
     }
 }
