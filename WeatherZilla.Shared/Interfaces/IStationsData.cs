8c91bec669dbcea3805a418c30708325ec7e02b6
shalafi <jorgen@bertholdsson.com>
2022-05-16 09:44:52 +0200

    Beginning of StationsData Need more knowledge and thought about this

---
+++ WeatherZilla.Shared/Interfaces/IStationsData.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherZilla.Shared.Interfaces
+{
+    public interface IStationsData
+    {
+        public DateTime Date { get; set; }
+
+        public int StationsId { get; set; }
+
+        public string? StationsName { get; set; }
+
+        public bool? Active { get; set; }
+
+    }
+}
---
+++ WeatherZilla.WebAPI/Controllers/StationsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherZilla.Shared.Data;
+
+namespace WeatherZilla.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StationsController : ControllerBase
+    {
+        private readonly ILogger<StationsController> _logger; // TODO: Use log feature
+
+        public StationsController(ILogger<StationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetStationData")]
+        public StationsData Get()
+        {
+            return new StationsData
+            {
+                Date = DateTime.Now,
+                StationsId = 148330,
+                StationsName = "Lycksele A",
+                Active = true,
+            };
+        }
+    }
+}
